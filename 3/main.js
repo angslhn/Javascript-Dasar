e967d76266c74a06b5baf66672c83975fda42ddc
angslhn <angslhn@gmail.com>
2024-10-10 16:31:32 +0700

    Belajar javascript dasar menggunakan typeof untuk mengecek tipe variable atau yang lainnya.

---
+++ 3/main.js
@@ -0,0 +1,21 @@
+// Typeof untuk mengecek tipe variable atau yang lainnya.
+let name = 'Aang Solihin';
+let age = 19;
+let height = 170.2;
+let gender = 'male';
+let email = 'angslhn@gmail.com';
+let online = true;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(gender);
+console.log(email);
+console.log(online);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof height);
+console.log(typeof gender);
+console.log(typeof email);
+console.log(typeof online);
