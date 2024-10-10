82094ba95b03ac53b78d9e1d348f40c1625d5a1b
angslhn <angslhn@gmail.com>
2024-10-10 16:28:03 +0700

    Belajar javascript dasar membuat variable.

---
+++ 2/main.js
@@ -0,0 +1,6 @@
+// Variable
+const myName = 'Aang Solihin';
+let myAge = 19;
+var myPassion = 'Programming';
+
+console.log(`Saya ${myName} berumur ${myAge} dan hal yang disukai saya adalah ${myPassion}.`);
