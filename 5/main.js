c82d03f4923da9c8d120dad37f3b7e40b4f8c86a
angslhn <angslhn@gmail.com>
2024-10-10 16:47:19 +0700

    Belajar javascript dasar semua operator.

---
+++ 5/main.js
@@ -0,0 +1,21 @@
+//  Semua operator
+//  +  = Tambah
+//  -  = Kurang
+//  *  = Kali
+//  /  = Bagi
+//  ** = Pangkat
+//  %  = Sisa Bagi
+
+let tambah = 15 + 15;
+let kurang = 20 - 15;
+let kali = 15 * 15;
+let bagi = 30 / 15;
+let pangkat = 2 ** 4;
+let sisaBagi = 30 % 2;
+
+console.log(tambah);
+console.log(kurang);
+console.log(kali);
+console.log(bagi);
+console.log(pangkat);
+console.log(sisaBagi);
