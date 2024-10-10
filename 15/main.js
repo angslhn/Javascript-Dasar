90bef5d278296553bca7460ee681bf4bb9e3b0fa
angslhn <angslhn@gmail.com>
2024-10-10 17:33:32 +0700

    Belajar javascript dasar operator perbandingan.

---
+++ 15/main.js
@@ -0,0 +1,33 @@
+// Operator perbandingan
+// =   Deklrasi
+// ==  Sama dengan
+// === Sama dengan tipe data dan nilai
+// !=  Tidak sama dengan
+// !== Tidak sama dengan tipe data dan nilai
+
+let angka = "10";
+let hewan = "Ayam";
+
+if (angka == 10) {
+    console.log("Benar!")
+} else {
+    console.log("Salah!")
+}
+
+if (angka === 10) {
+    console.log("benar!");
+} else {
+    console.log("Salah!")
+}
+
+if (hewan === "ayam") {
+    console.log("Benar!")
+} else {
+    console.log("Salah!")
+}
+
+if (hewan !== "Ayam") {
+    console.log("benar!");
+} else {
+    console.log("Salah!")
+}
