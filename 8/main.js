2e151282df827a0213768c9504f85186d81c1cf4
angslhn <angslhn@gmail.com>
2024-10-10 17:02:36 +0700

    Belajar javascript dasar if, else if, dan else statement.

---
+++ 8/main.js
@@ -0,0 +1,13 @@
+// if, else if, dan else statement
+
+let nilai = 80;
+
+if (nilai >= 80 && nilai <= 100) {
+  console.log("Nilai Baik.");
+} else if (nilai >= 60 && nilai <= 80) {
+  console.log("Nilai Cukup Baik.");
+} else if (nilai >= 0 && nilai <= 60) {
+  console.log("Nilai Kurang Baik.");
+} else {
+  console.log("Nilai Tidak Valid.");
+}
