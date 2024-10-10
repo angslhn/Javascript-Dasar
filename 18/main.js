3d0b39d88ab479d7c1932f8f748a697fffd81ac1
angslhn <angslhn@gmail.com>
2024-10-10 18:01:55 +0700

    Belajar javascript dasar function.

---
+++ 18/main.js
@@ -0,0 +1,10 @@
+// Function
+
+let x = parseInt(prompt("Angka Pertama"));
+let y = parseInt(prompt("Angka Kedua"));
+
+function tambahNumber(x, y) {
+  return x + y;
+}
+
+alert(`Hasilnya adalah ${tambahNumber(x, y)}`);
