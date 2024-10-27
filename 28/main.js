073585fa0b6f949da5f590678a689a6aa4c366d5
angslhn <angslhn@gmail.com>
2024-10-27 11:52:59 +0700

    Belajar javascript dasar higher-order function dan callback function.

---
+++ 28/main.js
@@ -0,0 +1,21 @@
+// Higher-order function dan Callback function
+// Function yang langsung di panggil setelah dibuat.
+
+function double (array, operasi) {
+    let result = [];
+    for (let toResult of array) {
+        result.push(operasi(toResult));
+    }
+    return result;
+}
+
+function kali(a) {
+    return a * 2
+} 
+
+let array = new Array(1, 2, 3, 4, 5);
+let result = double(array, kali);
+
+for (value of result) {
+    console.log(value);
+}
