5d2358a3102316e6b79f0a93db59cc028e82d81d
angslhn <angslhn@gmail.com>
2024-11-03 11:17:46 +0700

    Belajar javascript dasar Polymorphismn.

---
+++ 34/main.js
@@ -0,0 +1,27 @@
+// Polymorphism
+
+class Hewan {
+    suara() {
+        return "Hewan Bersuara";
+    }
+}
+
+class Kucing extends Hewan {
+    suara() {
+        return "Meow meowww!"
+    }
+}
+
+class Anjing extends Hewan {
+    suara() {
+        return "Guuk guuk!"
+    }
+}
+
+let hewan = new Hewan();
+let kucing = new Kucing();
+let anjing = new Anjing();
+
+console.log(hewan.suara());
+console.log(kucing.suara());
+console.log(anjing.suara());
