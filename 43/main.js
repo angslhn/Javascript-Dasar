c830d098492e636f09272112892fb8c1110f5ed9
angslhn <angslhn@gmail.com>
2024-11-07 19:49:44 +0700

    Belajar javascript dasar promises axios biasa.

---
+++ 43/main.js
@@ -0,0 +1,12 @@
+// Asynchronus promises axios biasa
+
+const id = Math.floor(Math.random() * (100 + 1));
+
+axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+.then((response) => {
+    console.log(`Title : ${response.data.title}`);
+    console.log(`Body : ${response.data.body}`);
+})
+.catch((error) => {
+    console.error(error);
+});
