2b3ada23f9faad089d971aa1e97fe19a791bd399
angslhn <angslhn@gmail.com>
2025-05-24 09:03:49 +0700

    Javascript Dasar.

--- 25. Array/main.js
+++ 25. Array/main.js
@@ -1,5 +1,6 @@
 // Array
 
+const users = ["Aang Solihin", "Maman Hidayat", "Deden Sukandi"]
 const semuaAngka = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 const semuaBuah = ["Apel", "Jeruk", "Mangga", "Pepaya", "Pisang", "Anggur"];
 
@@ -10,3 +11,20 @@ for (let angka of semuaAngka) {
 for (let buah of semuaBuah) {
   console.log(buah)
 }
+
+// Menyalin Array lain
+
+const customers = Array.from(users)
+console.log(customers)
+
+// Array Destructuring
+
+const Auth = ["customer", true]
+
+const [role, isLogin] = Auth
+
+const infoAuth = `Pengguna ${isLogin ? "telah login" : "belum login"}`
+const infoRole = "Role pengguna adalah" + " " + role
+
+console.log(infoAuth)
+console.log(infoRole)
